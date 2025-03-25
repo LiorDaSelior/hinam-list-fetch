94d3a9526cd4a866d1afbad94fafb2c96585fb58
LiorDaSelior <liorbelenkov@mail.tau.ac.il>
2025-03-25 16:04:13 +0200

    added auto config

--- src/main/java/com/hinam_list/hinam_list_fetch/HinamListFetcherApplication.java
+++ src/main/java/com/hinam_list/hinam_list_fetch/HinamListFetcherApplication.java
@@ -1,8 +1,10 @@
 package com.hinam_list.hinam_list_fetch;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class HinamListFetcherApplication {
 
