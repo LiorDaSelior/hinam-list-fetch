ef4f2282e457433c7ebada8017b96100eca9742a
LiorDaSelior <liorbelenkov@mail.tau.ac.il>
2025-03-25 15:06:22 +0200

    init

---
+++ src/main/java/com/hinam_list/hinam_list_fetch/HinamListFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.hinam_list.hinam_list_fetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HinamListFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HinamListFetcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hinam_list/hinam_list_fetch/HinamListFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hinam_list.hinam_list_fetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HinamListFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
