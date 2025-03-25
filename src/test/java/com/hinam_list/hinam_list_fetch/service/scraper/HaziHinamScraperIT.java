655a80de0950530337b9757647f3126bd93f51c2
LiorDaSelior <liorbelenkov@mail.tau.ac.il>
2025-03-25 19:04:07 +0200

    added file support

--- src/test/java/com/hinam_list/hinam_list_fetch/service/scraper/HaziHinamScraperIT.java
+++ src/test/java/com/hinam_list/hinam_list_fetch/service/scraper/HaziHinamScraperIT.java
@@ -6,13 +6,15 @@ import com.hinam_list.hinam_list_fetch.service.scraper.exception.APIResponseExce
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.TestPropertySource;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.ok;
@@ -26,9 +28,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class HaziHinamScraperIT {
     @Autowired
     protected HaziHinamScraper scraper;
+    protected final String jsonResponseString;
+
 
     public HaziHinamScraperIT() {
         addInitStub();
+
+        Resource storeApiResponseFile = new DefaultResourceLoader().getResource("StoreAPIExample-HaziHinam.json");
+        try {
+            jsonResponseString =  new String(
+                    storeApiResponseFile.getInputStream().readAllBytes(),
+                    StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @RegisterExtension
@@ -53,24 +66,10 @@ public class HaziHinamScraperIT {
         wireMockServer.stubFor(WireMock.get(urlPathEqualTo("/api/Catalog/get"))
                 .willReturn(
                 ok(
-                        """
-                                        {
-                                            "Results" : {
-                                                 "Categories": [
-                                                    {
-                                                        "SubCategories": [
-                                                        {"Id" : 0},
-                                                        {"Id" : 1},
-                                                        {"Id" : 2}
-                                                        ]
-                                                    }
-                                                ]
-                                            }
-                                        }
-                                        """
+                        jsonResponseString
                 )));
 
         var result = this.scraper.getCategoryIdList();
-        assertEquals(Arrays.asList("0", "1", "2"), result);
+        assertEquals(Arrays.asList("0", "1", "2", "3"), result);
     }
 }
